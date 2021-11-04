1f335316cafe780b26f822cef3589dfa5258235a
thousandyoung <muji_cullin@qq.com>
2021-11-04 15:38:40 +0800

    完善了对红色指针的支持

--- reinforce_color.py
+++
@@ -1,8 +0,0 @@
-import os
-import cv2
-import numpy as np
-#--- performs Otsu threshold ---
-def threshold(img, st):
-    ret, thresh = cv2.threshold(img, 0, 255,  cv2.THRESH_OTSU|cv2.THRESH_BINARY )
-    return  thresh
-
