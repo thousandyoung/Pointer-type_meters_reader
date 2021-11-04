a13e588c022358c02f01f650fe8772e82c8b9915
thousandyoung <muji_cullin@qq.com>
2021-11-04 10:31:59 +0800

    新增了对各种颜色指针的支持、以及指针选取最长的一条

--- reinforce_color.py
+++ reinforce_color.py
@@ -3,6 +3,8 @@ import cv2
 import numpy as np
 #--- performs Otsu threshold ---
 def threshold(img, st):
-    ret, thresh = cv2.threshold(img, 0, 255,  cv2.THRESH_OTSU)
-    return  thresh
+    binary = cv2.adaptiveThreshold(img, 255,
+                                   cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 15, -10)
+    # ret, thresh = cv2.threshold(img, 0, 255,  cv2.THRESH_OTSU|cv2.THRESH_BINARY)
+    return  img
 
